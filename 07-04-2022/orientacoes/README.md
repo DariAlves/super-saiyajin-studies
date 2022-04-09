da65140ec15acc2545a1f8accf0a14b5d8d1c8aa
Dari Alves <darieltondf@gmail.com>
2022-04-09 11:08:42 -0300

    :sunglasses: orientacoes

---
+++ 07-04-2022/orientacoes/README.md
@@ -0,0 +1,15 @@
+## Média dos Alunos
+
+#### Referências 
+
+[document.getElementById()](https://developer.mozilla.org/pt-BR/docs/Web/API/Document/getElementById)
+
+[element.addEventListener()](https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener)
+
+[Number()](https://www.w3schools.com/jsref/jsref_number.asp)
+
+[textContent](https://developer.mozilla.org/pt-BR/docs/Web/API/Node/textContent)
+
+[HTMLElement.style](https://www.w3schools.com/jsref/prop_style_color.asp)
+
+[inputText.value](https://www.w3schools.com/Jsref/prop_text_value.asp)
---
+++ 07-04-2022/orientacoes/index.js
@@ -0,0 +1,28 @@
+// Capturar elementos HTML
+
+// Criar um evento de clique
+//  A mensagem e a cor devem variar de acordo com a nota do aluno, seguindo as seguintes condições:
+
+/*
+    Condição: Se o valor da nota não for do tipo 'number'. Faça uso da função nativa isNaN().
+    Mensagem:  Digite um valor válido, fera!
+    Cor do texto: gray
+*/
+
+/*
+    Condição: Se o varlor da nota for >= 7.
+    Mensagem:  Parabéns, jovem ninja. Você foi aprovado!
+    Cor do texto: green
+*/
+
+/*
+    Condição: Se o varlor da nota for >= 5.
+    Mensagem:  Rapadura é doce, mas não é mole não. Você ficou de recuperação!
+    Cor do texto: orange
+*/
+
+/*
+    Condição: Se condição não se encaixar em nenhuma das anteriores.
+    Mensagem:  Que burro!!Dá 0 pra ele! Você foi reprovado!
+    Cor do texto: red
+*/
