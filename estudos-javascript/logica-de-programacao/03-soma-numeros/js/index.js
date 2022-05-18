728c5c79ed0941e040b3796ad5fc0ff3035e809e
Dari Alves <darieltondf@gmail.com>
2022-05-18 12:05:50 -0300

    :sunglasses: metodos de array e loops

---
+++ estudos-javascript/logica-de-programacao/03-soma-numeros/js/index.js
@@ -0,0 +1,85 @@
+// let valorA = 3;
+// let valorB = 7;
+// let total = 0;
+
+// while (valorA <= valorB) {
+//   total += valorA;
+//   valorA += 1;
+// }
+
+// for (let i = valorA; i <= valorB; i++) {
+//   total += i;
+// }
+
+// console.log(total);
+
+// for
+// for (let i = 0; i < usuarios.length; i++) {
+//   console.log(usuarios[i]);
+// }
+
+// forEach
+// usuarios.forEach(usuario => console.log(usuario));
+
+// for...of
+// for (const usuario of usuarios) {
+//   console.log(usuario);
+// }
+
+// for...in
+// for (const index in usuarios) {
+//   console.log(usuarios[index].nome);
+// }
+
+// const findUsuario = usuarios.find(usuario => usuario.id === 2);
+
+// console.table(findUsuario);
+
+// const indexUsuario = usuarios.findIndex(usuario => usuario.id === 2);
+
+// console.log(indexUsuario);
+
+// console.log(usuarios[indexUsuario]);
+
+// const usuarios = [
+//   {
+//     id: 1,
+//     nome: 'goku',
+//     raca: 'saiyajin',
+//   },
+//   {
+//     id: 2,
+//     nome: 'vegeta',
+//     raca: 'saiyajin',
+//   },
+//   {
+//     id: 3,
+//     nome: 'kuririn',
+//     raca: 'humano',
+//   },
+//   {
+//     id: 4,
+//     nome: 'gohan',
+//     raca: 'saiyajin',
+//   },
+//   {
+//     id: 5,
+//     nome: 'picollo',
+//     raca: 'namekuseijin',
+//   },
+// ];
+
+// const arraySaiyajin = usuarios.filter(usuario => usuario.raca !== 'saiyajin');
+
+// console.table(arraySaiyajin);
+
+// const salarioSemanalMaio = [500, 800, 1000, 650];
+// let saldoBancario = 2500;
+
+// const somaNumeros = salarioSemanalMaio.reduce((valorPrev, valorAtual) => {
+//   console.log(`Valor prévio: ${valorPrev}`);
+//   console.log(`Valor atual: ${valorAtual}`);
+//   return valorPrev + valorAtual;
+// }, saldoBancario);
+
+// console.log(somaNumeros);
